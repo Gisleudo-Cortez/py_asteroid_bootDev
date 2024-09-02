9f2cf2ae0e67218ef899fd8f0625c28181617178
Gisleudo <gisleudo.cmf@gmail.com>
2024-09-02 15:14:50 -0300

    Create asteroids and AsteroidField

--- main.py
+++ main.py
@@ -1,26 +1,63 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
+    # Initialize pygame
     pygame.init()
+
+    # Create screen canvas
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Create clock object to control fps
     clock = pygame.time.Clock()
+
+    # Create update and draw Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Add player to groups
+    Player.containers = (updatable, drawable)
+
+    # Add asteroids to groups
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    # Add Asteroid field to updatable
+    AsteroidField.containers = updatable
+
+    # Instanciate AsteroidField
+    asteroid_field = AsteroidField()
+
+    # Instanciate player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # initialize delta time variable for frame control
     dt = 0
 
+    # main game loop
     while True:
+
+        # check for close window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Control canvas display and updates
+        for obj in updatable:
+            obj.update(dt)
+        
+        screen.fill("black") # draw black screen
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
+        pygame.display.flip() # refresh screen
 
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-
-        # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -18,3 +18,25 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotated(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotated(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotated(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
