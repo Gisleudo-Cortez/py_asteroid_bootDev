8a4d100ad69dd426013b1dfa60a0c7d275931e96
Gisleudo <gisleudo.cmf@gmail.com>
2024-09-02 09:43:54 -0300

    Initialize screen rendering with infinite loop

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from constants import *
 import pygame
 
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        # listen for close
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # Draw black screen
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip() # update frame
 
 if __name__ == "__main__":
     main()
